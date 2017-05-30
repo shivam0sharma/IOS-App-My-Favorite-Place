7a63fedf0f2e9e104f5464b5465452a08cee25e8
Shaisan2 <shivam0sharma@icloud.com>
2017-05-30 13:44:46 -0400

    Added the graphics with constraints.

--- My-Favorite-Place/ViewController.swift
+++ My-Favorite-Place/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var moreButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        moreButton.layer.cornerRadius = 2.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func moreButton(_ sender: UIButton) {
+        
+    }
 
 }
 
